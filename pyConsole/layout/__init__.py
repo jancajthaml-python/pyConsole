118a44fb8ed75ee2470122d7ab23fc34d136d7ef
Jan Cajthaml <jan.cajthaml@abakus.com>
2016-04-23 20:35:33 +0200

    fixed dead import

--- pyConsole/layout/__init__.py
+++ pyConsole/layout/__init__.py
@@ -1,5 +1,4 @@
 from .main import MainScreen
-from .void import VoidScreen
 from .vertical_scroll import VerticalScrollableScreen
 from .horizontal_split import HorizontalSplitScreen
 from .vertical_split import VerticalSplitScreen
